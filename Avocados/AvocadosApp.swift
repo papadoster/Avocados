812ea61eb692b41f8d63a7f1a149a1012aacaf0d
Aleksandr Karpov <papadoster13@gmail.com>
2023-03-14 20:54:24 +0100

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Александр Карпов on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
